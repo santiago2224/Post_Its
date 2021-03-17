8d33a318ad14568e9af4b06f48745d393718953e
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-03-17 15:00:39 -0600

    crud controllers done

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,48 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+
+    render component: "Note", props: {notes: @notes}
+  end
+
+  def show
+    @note = Note.find(params[:id])
+
+    render component: "Note", props: {note: @note}
+  end
+
+  def new
+    render component: "NewNote"
+  end
+
+  def create
+    Note.create(note_params)
+    redirect_to notes_path
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+    render component: "EditNote", props: {note: @note}
+  end
+
+  def updated
+    @note = Note.find(params[:id])
+
+    @note.update(note_params)
+    redirect_to notes_path
+  end
+  
+  def destroy
+    @note = Note.find(params[:id])
+
+    @note.destroy(note_params)
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+  pramas.require(:note).permit(:title, description)
+  end
+
+end
